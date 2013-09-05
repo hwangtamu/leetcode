dd0725084f9516a6d63703ab70ace6228e138af1
Han Wang <hanwang@tamu.edu>
2013-09-05 11:36:51 -0500

    Create twoSum_HanWang.cpp

---
+++ Two_Sum/twoSum_HanWang.cpp
@@ -0,0 +1,33 @@
+// twosum.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	vector<int> twoSum(vector<int> &numbers, int target)
+	{
+		vector<int> sum(numbers.size(),0);
+		vector<int> ans(2,0);
+		for(int i=0;i<numbers.size();++i)
+		{
+			ans[0] = i+1;
+			ans[1] = ans[0]+1;
+			while(ans[1]<=numbers.size())
+			{
+				if(numbers[ans[0]-1]+numbers[ans[1]-1]==target)
+					return ans;
+				++ans[1];
+			}
+		}
+	}
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
